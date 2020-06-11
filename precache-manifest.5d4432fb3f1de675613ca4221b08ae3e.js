c6e88ec51bed3204b884c68f81cbc6967961dab8
Chirag <chirag00757@yahoo.in>
2020-06-11 00:37:20 -0500

    Updates

---
+++ precache-manifest.5d4432fb3f1de675613ca4221b08ae3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87697aa6f4188e261eb2b473e0dce2c0",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "0c694f25ce52ed83d6fb",
+    "url": "/todoapp/static/css/main.efa2dfcf.chunk.css"
+  },
+  {
+    "revision": "3bc48cfffd185119713d",
+    "url": "/todoapp/static/js/2.03e39d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.03e39d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c694f25ce52ed83d6fb",
+    "url": "/todoapp/static/js/main.93d7f9e5.chunk.js"
+  },
+  {
+    "revision": "bc7f15e0b2035d7f0465",
+    "url": "/todoapp/static/js/runtime-main.243e3f1d.js"
+  }
+]);
